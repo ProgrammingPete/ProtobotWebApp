15fa94021842076a5908997e2e7197fdc1f7e27f
TonyD42 <Tony42D@gmail.com>
2019-01-29 16:54:43 -0500

    ProtobotWebApp

--- ProtobotWebApp/authentication.py
+++ ProtobotWebApp/authentication.py
@@ -1,8 +1,24 @@
 import os
 import bcrypt
 salt = bcrypt.gensalt()
-password = 'Tony is the best'
+password = 'testing'
+print("whats is your name?: ")
+name = input()
 
 hashValue = bcrypt.hashpw(password.encode('utf-8'), salt)
 print(salt)
 print(hashValue)
+
+storage = {'Name': name, 'Hash': hashValue, 'Salt': salt}
+
+print("What is your password?: ")
+passwdInput = input()
+
+compareValue = bcrypt.hashpw(passwdInput.encode('utf-8'), salt)
+
+if compareValue == hashValue:
+	print(compareValue)
+	print("Password Verified")
+
+else:
+	print("Wrong Password")
