22d1a40b0ad81e20758939915b26b415cb272189
Peter Parianos <peter.parianos@yahoo.com>
2019-02-16 23:36:03 -0500

    cleaned DB, addedd auth error catching and authenitcation through database

--- ProtobotWebApp/authentication.py
+++ ProtobotWebApp/authentication.py
@@ -11,14 +11,17 @@ def createUser(userName, password):
     credentialStorage[userName] = {'hashValue': hashValue, 'salt': salt}
 
     #add to database
-    newUser = User(username = userName, hashvalue = hashValue.decode('utf-8'), password_salt = salt.decode('utf-8'))
+    newUser = User(username = userName, hashvalue = hashValue, password_salt = salt)
     db.session.add(newUser)
     db.session.commit()
 
 def validation(username, password):
-	compareValue = bcrypt.hashpw(password.encode('utf-8'), credentialStorage[username]['salt'])
-	#Need to add section here to catch keyerrors i.e. the user does not exist
-	if compareValue == credentialStorage[username]['hashValue']:
-		return 1
-	else:
-		return 0
+    try:
+        currentuser = User.query.filter_by(username = username).first()
+        compareValue = bcrypt.hashpw(password.encode('utf-8'), currentuser.password_salt)
+        if compareValue == currentuser.hashvalue:
+            return 1
+        else:
+            return 0
+    except AttributeError:
+        return 0
